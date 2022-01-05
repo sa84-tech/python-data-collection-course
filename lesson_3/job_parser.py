b43e97337057f24c419c7f43c1cc95b745505c9a
sa84 <sa84@list.ru>
2022-01-05 22:28:07 +0300

    Task for lesson 3 completed

--- lesson_3/job_parser.py
+++ lesson_3/job_parser.py
@@ -17,22 +17,25 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gec
 DB_URI = os.environ.get('DB_URI')
 DB_NAME = os.environ.get('DB_NAME')
 
+if not (DB_URI and DB_NAME):
+    print('Add .env file with the variables DB_URI and DB_NAME.')
+    sys.exit(1)
+
 try:
     CLIENT = MongoClient(DB_URI)
     DB = CLIENT[DB_NAME]
-    print(DB)
 except Exception as e:
-    print(e)
+    print('Error:', e)
     sys.exit(1)
 
-vacancies_collection = DB.vacancies
+VAC_COLLECTION = DB.vacancies
 
 
 def parse_salary_string(salary_string):
-    salary = {}
+    salary = {'min': 0, 'max': 0, 'currency': None}
     ss = re.sub(r'\s+', '', salary_string, flags=re.UNICODE)
     if re.search('договор', salary_string):
-        salary['agreement'] = True
+        return salary
     else:
         try:
             values = [float(s) for s in re.findall(r'-?\d+\.?\d*', ss)]
@@ -47,7 +50,7 @@ def parse_salary_string(salary_string):
                 salary['min'] = values[0]
                 salary['max'] = values[1]
             else:
-                salary['exact'] = values[0]
+                salary['min'] = values[0]
 
     if re.search('руб.', salary_string):
         salary['currency'] = 'RUB'
@@ -71,139 +74,114 @@ def parse_page(dom_obj, vacancies):
         vacancies.append({'name': name, 'salary': salary, 'source': JOBLIST_URL, 'link': f'{JOBLIST_URL}{link}'})
 
 
-def is_new(vacancies):
+def insert_new_vacancies(collection, vacancies):
     new_vacancies = []
     for vacancy in vacancies:
-        if not vacancies_collection.find_one({'link': vacancy['link']}):
-            new_vacancies.append(vacancy)
+        if not collection.find_one({'link': vacancy['link']}):
+            try:
+                collection.insert_one(vacancy)
+                new_vacancies.append(vacancy)
+            except Exception as ee:
+                print('Database Error:', ee)
+                return None
     return new_vacancies
 
 
-def update_db(collection, vacancies):
-    new_vacancies = []
-    print('Updating database...')
+def get_vacancies(collection, salary_value=0.0):
+    print('Min salary:', salary_value)
+    find_obj = {'$or': [
+        {'salary.min': {'$gte': salary_value}},
+        {'salary.max': {'$gte': salary_value}},
+    ]}
+    result = [item for item in collection.find(find_obj)]
+    return result
+
+
+def display_vacancies():
     try:
-        for vacancy in vacancies:
-            if not collection.find_one({'link': vacancy['link']}):
-                new_vacancies.append(vacancy)
-        created_vacancies = collection.insert_many(new_vacancies)
-    except Exception as ee:
-        print('Database Error: Something went wrong!')
-        print(ee)
-        return None
+        salary_value = float(input('Enter the value of the minimum salary: '))
+    except ValueError:
+        print('Incorrect value!')
+        return
+    result = get_vacancies(VAC_COLLECTION, salary_value)
+    if result:
+        pprint(result)
+        print(f'Total number of vacancies with salary above {salary_value}: {len(result)}')
     else:
-        print('Done.')
-        return created_vacancies
+        print('No vacancies')
 
 
-def get_vacancies(collection):
-    print(collection)
-    # try:
-    #     check_value = float(input('Enter minimum salary value: '))
-    # except ValueError:
-    #     print('Invalid salary value!')
-    #     return None
-    # obj = {'salary': {'$or': [
-    #     {'salary': {'min': {'$gte': 100000}}},
-    #     {'salary': {'max': {'$gte': 100000}}},
-    #     {'salary': {'exact': {'$gte': 100000}}}
-    # ]}}
+def fetch_vacancies():
+    position = input('Enter the name of the vacancy: ')
+    if len(position) < 3:
+        print('Minimum length - 3 characters')
+        return
 
-    # obj = {'min': {'$gte': 100000}}
-    # obj = {'agreement': True}
+    only_new = input('Type 1 for only new vacancies, 2 for all: ')
+    if only_new not in ('1', '2'):
+        print('Invalid input!')
+        return
 
-    obj = {'salary': {'agreement': True}}
-    # obj = {'salary': {'currency': "RUB", 'min': {'$gte': 10000}}}
-    # obj = {'salary': {'currency': "RUB", 'min': 100000}}
+    params = {'keywords': position}
+    vacancies = []
 
-    # obj = {'salary': {'currency': "RUB", 'min': 100000}}
-    print(obj)
-    for doc in collection.find(obj):
-        pprint(doc)
-    # print(query_result)
-    # result = collection.find({})
-    # answer = [item for item in query_result]
-    # pprint(answer)
+    response = requests.get(f"{JOBLIST_URL}/vacancy/search/", params=params, headers=HEADERS)
 
+    if response.ok:
+        print('Parsing in progress...')
+        dom = bs(response.text, 'html.parser')
+        pages = dom.find('a', {'class': 'f-test-link-Dalshe'})
+        parse_page(dom, vacancies)
+        if pages:
+            try:
+                max_pages = int(pages.previous_sibling.text)
+            except ValueError:
+                print(f'Warning: Unable to fetch the total number of pages.\nFirst page output:')
+            else:
+                print(f'page 1 of {max_pages} completed.', end='')
+                for page in range(2, max_pages + 1):
+                    params['page'] = page
+                    response = requests.get(f"{JOBLIST_URL}/vacancy/search/", params=params, headers=HEADERS)
+                    if response.ok:
+                        dom = bs(response.text, 'html.parser')
+                        parse_page(dom, vacancies)
+                        print(f'\rpage {page} of {max_pages} completed.  ', end='')
+                    else:
+                        print(f'Error: Unable to fetch data from page {page} of {max_pages}!\nPartial output:')
+                        break
+        print('\rDone! Processing result... ')
+
+        new_vacancies = insert_new_vacancies(VAC_COLLECTION, vacancies)
+
+        if only_new == '1':
+            print(f'New Vacancies List (position "{position}"):')
+            pprint(new_vacancies)
+        else:
+            print(f'Vacancies List (position "{position}"):')
+            pprint(vacancies)
+
+        print(f'Total number of vacancies for the position of an "{position}": {len(vacancies)}')
+        print(f'Number of new vacancies: {len(new_vacancies)}')
+
+    else:
+        print(f'Error: Something went wrong! Status code:', response.status_code)
 
 
 def main():
     print('SuperJob Vacancy Analyzer')
     while True:
-        menu_item = input('Type command: 1 - fetch new vacancies from source, '
-                          '2 - display vacancies from db, q - quit the program')
+        menu_item = input('Enter command: 1 - fetch new vacancies from SuperJob, '
+                          '2 - display vacancies from db, q - quit the program: ')
+
         if menu_item == 'q':
             print('Exit by user command.')
             break
-        elif menu_item == '2':
-            result = get_vacancies(vacancies_collection)
-            if result:
-                print(result)
-            else:
-                print('No vacancies')
 
         elif menu_item == '1':
-            position = input('Enter the name of the vacancy: ')
-            if len(position) < 3:
-                print('Minimum length - 3 characters')
-                continue
-            output = input('Type 1 for only new vacancies, 2 for all: ')
-            if output not in ('1', '2'):
-                print('Invalid input!')
-                continue
+            fetch_vacancies()
 
-            params = {'keywords': position}
-            vacancies = []
-
-            response = requests.get(f"{JOBLIST_URL}/vacancy/search/", params=params, headers=HEADERS)
-
-            if response.ok:
-                print('Parsing in progress...')
-                dom = bs(response.text, 'html.parser')
-                pages = dom.find('a', {'class': 'f-test-link-Dalshe'})
-                parse_page(dom, vacancies)
-                if pages:
-                    try:
-                        max_pages = int(pages.previous_sibling.text)
-                    except ValueError:
-                        print(f'Warning: Unable to fetch the total number of pages.\nFirst page output:')
-                    else:
-                        print(f'page 1 of {max_pages} completed.', end='\r')
-                        for page in range(2, max_pages + 1):
-                            params['page'] = page
-                            response = requests.get(f"{JOBLIST_URL}/vacancy/search/", params=params, headers=HEADERS)
-                            if response.ok:
-                                dom = bs(response.text, 'html.parser')
-                                parse_page(dom, vacancies)
-                                print(f'page {page} of {max_pages} completed.  ', end='\r')
-                            else:
-                                print(f'Error: Unable to fetch data from page {page} of {max_pages}!\nPartial output:')
-                                break
-                print('Done! Processing result... ', end='\r')
-                # new_vacancies = is_new(vacancies)
-                new_vacancies = update_db(vacancies_collection, vacancies)
-                if output == '1':
-                    print(f'New Vacancies List (position "{position}"):')
-                    pprint(new_vacancies)
-                else:
-                    print(f'Vacancies List (position "{position}"):')
-                    pprint(vacancies)
-
-                print(f'Total number of vacancies for the position of an "{position}": {len(vacancies)}')
-                new_vacancies_count = len(new_vacancies.inserted_ids) if new_vacancies else 0
-                print(f'Number of new vacancies: {new_vacancies_count}')
-                #
-                # print('Updating database...')
-                # try:
-                #     vacancies_collection.insert_many(new_vacancies)
-                # except Exception as ee:
-                #     print('Error: Something went wrong!')
-                #     print(ee)
-                # else:
-                #     print('Done.')
-
-            else:
-                print(f'Error: Something went wrong! Status code:', response.status_code)
+        elif menu_item == '2':
+            display_vacancies()
 
 
 if __name__ == '__main__':
