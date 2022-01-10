31c218e43236cf964c7cd07d4e49f111e4f90498
sa84 <sa84@list.ru>
2022-01-11 01:08:31 +0300

    lesson 5 complete

--- lesson_5/email_picker.py
+++ lesson_5/email_picker.py
@@ -1,7 +1,5 @@
 import os
 import sys
-from pprint import pprint
-from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,10 +8,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 from utils.db_init import MAIL_COLLECTION
+from utils.helpers import parse_date, get_id
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
@@ -36,32 +33,47 @@ elem = driver.find_element(By.XPATH, "//input[@name='login']")
 elem.send_keys(EMAIL_ADDRESS)
 elem.send_keys(Keys.ENTER)
 
-wait = WebDriverWait(driver, 10)
-
 elem = driver.find_element(By.XPATH, "//input[@name='password']")
 elem.send_keys(EMAIL_PASSWORD)
 elem.send_keys(Keys.ENTER)
 
-actions = ActionChains(driver)
 email_links = set()
 last_item = None
 
+print("Email Picker")
+
 while True:
     items = driver.find_elements(By.CSS_SELECTOR, 'a.js-letter-list-item')
-    print('len(items):', len(items))
     if last_item == items[-1]:
-        print('EXIT')
         break
     last_item = items[-1]
     for item in items:
         email_links.add(item.get_attribute('href'))
-    actions.move_to_element(items[-1])
-    actions.perform()
-    print(len(email_links), items[-1].text)
 
-print(len(email_links))
-pprint(email_links)
+    ActionChains(driver)\
+        .move_to_element(items[-1])\
+        .perform()
 
-sleep(10)
+    print(f'\rEmails count: {len(email_links)}', end='...')
+
+print(f'\rEmails count: {len(email_links)}    ')
+
+for i, email_link in enumerate(email_links, 1):
+    print(f'\rProcessing {i} email from {len(email_links)}', end='')
+
+    email = {'_id': get_id(email_link)}
+
+    if MAIL_COLLECTION.find_one(email):
+        continue
+
+    driver.get(email_link)
+    email['author'] = driver.find_element(By.CSS_SELECTOR, 'span.letter-contact').get_attribute('title')
+    email['date'] = parse_date(driver.find_element(By.CSS_SELECTOR, 'div.letter__date').text)
+    email['subject'] = driver.find_element(By.CSS_SELECTOR, 'h2.thread-subject').text
+    email['body'] = driver.find_element(By.CSS_SELECTOR, "div.letter__body").text
+
+    MAIL_COLLECTION.insert_one(email)
+
+print('\nDone.')
 
 driver.close()
