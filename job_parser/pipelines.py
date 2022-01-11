b362bebd3542b5bc5be2c5303d38527c49887850
sa84 <sa84@list.ru>
2022-01-12 01:35:57 +0300

    process_salary_hh added

--- job_parser/pipelines.py
+++ job_parser/pipelines.py
@@ -5,6 +5,8 @@
 
 
 # useful for handling different item types with a single interface
+import re
+
 from itemadapter import ItemAdapter
 import os
 
@@ -14,7 +16,6 @@ from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
 
 DB_URI = os.environ.get('DB_URI')
-DB_NAME = os.environ.get('DB_NAME')
 
 
 class JobParserPipeline:
@@ -23,10 +24,29 @@ class JobParserPipeline:
         self.mongobase = client.job_parser_scrapy
 
     def process_item(self, item, spider):
-        item['min'], item['max'], item['cur'] = self.process_salary(item['salary'])
+        item['min'], item['max'], item['cur'] = self.process_salary_hh(item['salary'])
         collection = self.mongobase[spider.name]
         collection.insert_one(item)
         return item
 
-    def process_salary(self, salary):
-        return 10, 20, 'руб.'
+    def process_salary_hh(self, salary):
+        salary_str = re.sub(r'\s+', '', ''.join(salary), flags=re.UNICODE)
+
+        if re.search('з/п', salary_str):
+            return 0.0, 0.0, None
+
+        min_sal = 0.0
+        max_sal = 0.0
+        cur_sal = salary[-2]
+
+        values = [float(s) for s in re.findall(r'-?\d+\.?\d*', salary_str)]
+
+        if re.search(r'от\d*до', salary_str):
+            min_sal = values[0]
+            max_sal = values[1]
+        elif re.search(r'от\d', salary_str):
+            min_sal = values[0]
+        elif re.search(r'до\d', salary_str):
+            max_sal = values[0]
+
+        return min_sal, max_sal, cur_sal
