c3aba9bef966d82eda8b5a67e40c96d47c5e931e
Андрей Громов <sa84@list.ru>
2022-01-13 17:28:27 +0300

    runner modified

--- job_parser/runner.py
+++ job_parser/runner.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     crawler_settings.setmodule(settings)
 
     process = CrawlerProcess(settings=crawler_settings)
-
+    process.crawl(HhruSpider)
     process.crawl(SuperjobruSpider)
+
     process.start()
